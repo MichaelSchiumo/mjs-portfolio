b3a9008bc705ddf3ba79b553b9e010298534a266
Michael <michaeljanschiumo@gmail.com>
2020-10-20 15:56:48 -0400

    added welcome component, animated logo

--- mjsportfolio/src/components/Welcome.js
+++ mjsportfolio/src/components/Welcome.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import title from '../images/title.gif';
 
 const Welcome = () => {
-  return <div>hi</div>;
+  return (
+    <div class='grid grid-cols-1 gap-4 justify-items-center h-48 mt-8'>
+      <img src={title} alt='' />
+      <div className='bg-white'>
+        <div className='max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:-mt-16'>
+          <div className='text-center grid grid-cols-1'>
+            <h1 className='text-base leading-6 font-semibold text-indigo-600 tracking-wide uppercase'>
+              Full Stack Developer
+            </h1>
+            <p className='mt-1 text-4xl leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl'>
+              Building elegant solutions to complex problems.
+            </p>
+            <p className='max-w-xl mt-5 mx-auto text-xl leading-7 text-gray-500'></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
