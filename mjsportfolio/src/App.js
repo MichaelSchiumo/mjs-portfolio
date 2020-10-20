b62fa22e911a2de2caa56b25dc059cf5573474ae
Michael <michaeljanschiumo@gmail.com>
2020-10-20 13:28:29 -0400

    added react-router-dom, added welcome component

--- mjsportfolio/src/App.js
+++ mjsportfolio/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './styles/main.css';
 import Navbar from './components/Navbar.js';
+import { Route, Switch } from 'react-router-dom';
+import Welcome from './components/Welcome.js';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Switch>
+        <Route exact path='/' component={Welcome} />
+      </Switch>
     </div>
   );
 }
