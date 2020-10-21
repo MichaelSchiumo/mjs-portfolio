ece0301d0815421546450a202828100a3a18f898
Michael <michaeljanschiumo@gmail.com>
2020-10-21 19:17:15 -0400

    added hover effect

--- mjsportfolio/src/components/edu/Finance.js
+++ mjsportfolio/src/components/edu/Finance.js
@@ -3,7 +3,7 @@ import tcnj from '../../images/tcnj-logo.png';
 
 const Finance = () => {
   return (
-    <div class='relative bg-gray-800'>
+    <div class='relative bg-gray-800 hover:bg-gray-700'>
       <div class='h-12 bg-white md:absolute md:h-full md:w-1/2'>
         <img
           class='w-1/2 h-1/2 object-cover ml-32 -mt-16'
@@ -14,7 +14,7 @@ const Finance = () => {
       <div class='relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-8'>
         <div class='md:ml-auto md:w-1/2 md:pl-10'>
           <div class='text-base leading-6 font-semibold uppercase tracking-wider text-gray-300 italic'>
-            Bachelor of Science
+            Bachelor of Science (2012-2016)
           </div>
           <h2 class='mt-2 text-white text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10 font-legal'>
             Finance
