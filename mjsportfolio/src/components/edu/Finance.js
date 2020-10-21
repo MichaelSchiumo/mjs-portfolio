c978f99786587741c4d66a6162519acc9abd17c5
Michael <michaeljanschiumo@gmail.com>
2020-10-21 17:49:34 -0400

    TCNJ logo centered

--- mjsportfolio/src/components/edu/Finance.js
+++ mjsportfolio/src/components/edu/Finance.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import tcnj from '../../images/tcnj-logo.png';
 
 const Finance = () => {
   return (
     <div class='relative bg-gray-800'>
-      <div class='h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2'>
+      <div class='h-12 bg-white md:absolute md:h-full md:w-1/2'>
         <img
-          class='w-full h-full object-cover'
-          src='https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6875F5&sat=-100&blend-mode=multiply'
+          class='w-1/2 h-1/2 object-cover ml-32 -mt-16'
+          src={tcnj}
           alt='Support team'
         />
       </div>
-      <div class='relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16'>
+      <div class='relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-8'>
         <div class='md:ml-auto md:w-1/2 md:pl-10'>
           <div class='text-base leading-6 font-semibold uppercase tracking-wider text-gray-300'>
             Award winning support
