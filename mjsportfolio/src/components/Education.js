ebea6f9303d35396c9fd797242f374d16374108a
Michael <michaeljanschiumo@gmail.com>
2020-10-21 17:56:28 -0400

    adjusted quote over education

--- mjsportfolio/src/components/Education.js
+++ mjsportfolio/src/components/Education.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Finance from './edu/Finance.js';
-import IconBar from './IconBar';
 
 const Education = () => {
   return (
@@ -9,10 +8,10 @@ const Education = () => {
         id='container'
         class='grid grid-cols-1 gap-4 justify-items-center p-2 h-36 mt-6 lg:p-0'
       >
-        <div className='bg-white'>
+        <div className='bg-white -mb-10'>
           <div className='lg:w-3/4 mx-auto py-16 px-4 sm:py-24 sm:px-6 sm:-mt-16 lg:px-8 lg:-mt-24'>
             <div className='text-center grid grid-cols-1'>
-              <blockquote className='mt-1 text-xl leading-6 font-extrabold text-gray-900 sm:leading-none sm:tracking-tight lg:text-3xl font-legal italic'>
+              <blockquote className='mt-1 text-xl leading-6 font-extrabold text-gray-900 sm:leading-none sm:tracking-tight lg:text-2xl font-legal italic'>
                 “Education is the most powerful weapon which you can use to
                 change the world.” <br />
                 <p>- Nelson Mandela</p>
