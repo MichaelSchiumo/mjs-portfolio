09664727e840cd759e64ede27146b102d4e313f3
Michael <michaeljanschiumo@gmail.com>
2020-10-22 12:07:26 -0400

    added 'home' link

--- mjsportfolio/src/components/Navbar.js
+++ mjsportfolio/src/components/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = () => {
             <div className='hidden sm:block sm:ml-6'>
               <div className='flex'>
                 <a
-                  href='#'
+                  href='/'
                   className='px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
                 >
                   Home
@@ -83,7 +83,7 @@ const Navbar = () => {
                   href='#'
                   className='ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
                 >
-                  Calendar
+                  Experience
                 </a>
               </div>
             </div>
