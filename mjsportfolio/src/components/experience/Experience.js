039f4e289511dd7a7b1a73f4d7041cc96ee0e096
Michael <michaeljanschiumo@gmail.com>
2020-10-22 17:12:33 -0400

    finished cards with proper links, adding text

--- mjsportfolio/src/components/experience/Experience.js
+++ mjsportfolio/src/components/experience/Experience.js
@@ -4,6 +4,7 @@ import usDept from '../../images/dept-state.jpg';
 import bofa2 from '../../images/bofa2.jpg';
 import amcham from '../../images/amcham.jpg';
 import vivint from '../../images/vivint.png';
+import ameriprise from '../../images/ameriprise.jpg';
 
 const Experience = () => {
   return (
@@ -188,36 +189,23 @@ const Experience = () => {
             </li>
             <li class='py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left'>
               <div class='space-y-6 xl:space-y-10'>
-                <img
-                  class='mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
-                  alt=''
-                />
+                <a href='https://www.ameriprise.com/'>
+                  <img
+                    class='mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56'
+                    src={ameriprise}
+                    alt=''
+                  />
+                </a>
                 <div class='space-y-2 xl:flex xl:items-center xl:justify-between'>
                   <div class='font-medium text-lg leading-6 space-y-1'>
-                    <h4 class='text-white'>Leonard Krasner</h4>
-                    <p class='text-indigo-400'>Senior Designer</p>
+                    <h4 class='text-white'>Finance Intern</h4>
+                    <p class='text-indigo-400'>Ameriprise Financial</p>
                   </div>
 
                   <ul class='flex justify-center space-x-5'>
                     <li>
                       <a
-                        href='#'
-                        class='text-gray-400 hover:text-gray-300 transition ease-in-out duration-150'
-                      >
-                        <span class='sr-only'>Twitter</span>
-                        <svg
-                          class='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'
-                        >
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='#'
+                        href='https://www.linkedin.com/company/ameriprise-financial-services-llc/'
                         class='text-gray-400 hover:text-gray-300 transition ease-in-out duration-150'
                       >
                         <span class='sr-only'>LinkedIn</span>
