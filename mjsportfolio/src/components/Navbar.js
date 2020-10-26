e624f2de1c993929e550ae40c590f79269f59592
Michael <michaeljanschiumo@gmail.com>
2020-10-26 16:54:39 -0400

    removed outline on home button in nav

--- mjsportfolio/src/components/Navbar.js
+++ mjsportfolio/src/components/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
               <div className='flex'>
                 <a
                   href='/'
-                  className='px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
+                  className='ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
                 >
                   Home
                 </a>
