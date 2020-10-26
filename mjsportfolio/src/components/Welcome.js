595af6098f9b96a4fdeaa1e0cf6b57aafa828498
Michael <michaeljanschiumo@gmail.com>
2020-10-26 18:00:31 -0400

    removed logo cloud from welcome

--- mjsportfolio/src/components/Welcome.js
+++ mjsportfolio/src/components/Welcome.js
@@ -7,14 +7,8 @@ import Slider from 'react-animated-slider';
 import LogoCloud from './LogoCloud';
 
 const Welcome = () => {
-  const slides = [
-    { title: 'First item', description: 'Lorem ipsum' },
-    { title: 'Second item', description: 'Lorem ipsum' },
-    { title: 'Third item', description: 'Lorem ipsum', img: Project },
-  ];
-
   return (
-    <div className='text-center grid grid-cols-1'>
+    <div className='text-center grid lg:grid-cols-1'>
       <section class='bg-white overflow-hidden'>
         <div class='relative max-w-screen-xl mx-auto pt-10 pb-2 px-4 sm:px-6 lg:px-16 lg:py-10'>
           <div class='relative lg:flex lg:items-center'>
@@ -40,7 +34,7 @@ const Welcome = () => {
                 <div class='max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
                   <div class='text-center'>
                     <p class='mt-1 text-4xl leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl lg:mb-10'>
-                      I build elegant solutions to complex problems.
+                      I Build Elegant Solutions to Complex Problems.
                     </p>
                     <div class='max-w-xl mt-5 mx-auto text-xl leading-7 text-gray-500'>
                       <Typing speed={50}>
@@ -65,9 +59,6 @@ const Welcome = () => {
           </div>
         </div>
       </section>
-      <div className='object-contain'>
-        <LogoCloud />
-      </div>
     </div>
   );
 };
