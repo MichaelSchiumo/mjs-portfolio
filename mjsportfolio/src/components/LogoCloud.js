d21174dce3a89735b0f9dc6ad64420a7fdb07eb9
Michael <michaeljanschiumo@gmail.com>
2020-10-25 21:55:07 -0400

    fixed logo cloud for projects page

--- mjsportfolio/src/components/LogoCloud.js
+++ mjsportfolio/src/components/LogoCloud.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LogoCloud = () => {
   return (
-    <div className='grid grid-rows-2 lg:mt-2 h-48 rounded-lg'>
+    <div className='grid grid-rows-2 h-60 mt-10 -mb-40 lg:mt-2 lg:h-48 rounded-lg'>
       <div class='bg-gray-800'>
         <div class='max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-6 lg:w-3/4'>
           <div class='grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5'>
@@ -40,7 +40,7 @@ const LogoCloud = () => {
         </div>
       </div>
       <div class='bg-gray-800'>
-        <div class='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-6 lg:w-full lg:mt-6'>
+        <div class='max-w-screen-xl mx-auto px-4 sm:px-6 mt-10 lg:px-6 lg:w-full lg:mt-6'>
           <div class='grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5'>
             <div class='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
               <span className='devicon-html5-plain fa-3x text-2xl text-white'>
