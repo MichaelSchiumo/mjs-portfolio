e7ca57d2e78dd1aa6ca894b0a1de0ab42c046659
Michael <michaeljanschiumo@gmail.com>
2020-10-27 11:21:07 -0400

    removed mobile collapsible menu

--- mjsportfolio/src/components/Navbar.js
+++ mjsportfolio/src/components/Navbar.js
@@ -6,42 +6,7 @@ const Navbar = () => {
     <nav className='bg-gray-800'>
       <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
         <div className='relative flex items-center justify-between h-16'>
-          <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-            <button
-              className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out'
-              aria-label='Main menu'
-              aria-expanded='false'
-            >
-              <svg
-                className='block h-6 w-6'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16M4 18h16'
-                />
-              </svg>
-              <svg
-                className='hidden h-6 w-6'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M6 18L18 6M6 6l12 12'
-                />
-              </svg>
-            </button>
-          </div>
+          <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'></div>
           <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
             <div className='flex-shrink-0'>
               <a href='/'>
@@ -108,41 +73,13 @@ const Navbar = () => {
                   className='flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out'
                   id='user-menu'
                   aria-label='User menu'
-                  aria-haspopup='true'
+                  aria-haspopup='false'
                 ></button>
               </div>
 
               <div className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg'></div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className='hidden sm:hidden'>
-        <div className='px-2 pt-2 pb-3'>
-          <a
-            href='#'
-            className='block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
-          >
-            Dashboard
-          </a>
-          <a
-            href='#'
-            className='mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
-          >
-            Team
-          </a>
-          <a
-            href='#'
-            className='mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
-          >
-            Projects
-          </a>
-          <a
-            href='#'
-            className='mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out'
-          >
-            Calendar
-          </a>
         </div>
       </div>
     </nav>
