35b6a8e6f6047cd5f888f93d5aa30550fc2e94df
Michael <michaeljanschiumo@gmail.com>
2020-10-28 13:20:33 -0400

    fixed title

--- mjsportfolio/src/components/Blog.js
+++ mjsportfolio/src/components/Blog.js
@@ -11,7 +11,7 @@ import Footer from './Footer.js';
 
 const Blog = () => {
   return (
-    <div class='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
+    <div class='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-32'>
       <div class='absolute inset-0'>
         <div class='bg-white h-1/3 sm:h-2/3'></div>
       </div>
